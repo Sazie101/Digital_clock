41f473ff2624e2720a3fb98b608e1e0cb13aeb0e
Samuel Yazie <samuelwassie169@gmail.com>
2023-11-05 19:59:34 -0600

    Fixing small bug

--- Digital-clock/assets/script/digital-clock.js
+++ Digital-clock/assets/script/digital-clock.js
@@ -30,13 +30,13 @@ function clockChange() {
     let hours = now.getHours(); 
     let minutes = now.getMinutes(); 
     let seconds = now.getSeconds(); 
-    hours = hours < 10 ? `0${hours}`: hours;
     minutes = minutes < 10 ? `0${minutes}` : minutes;
     seconds = seconds < 10 ? `0${seconds}` : seconds;
     let ampm = hours >= 12 ? 'PM' : 'AM';
     if (hours > 12){
         hours -= 12;
     }
+    hours = hours < 10 ? `0${hours}`: hours;
     let showHours = selectById('hours');
     let showMins = selectById('minutes');
     let showSecs = selectById('seconds');
